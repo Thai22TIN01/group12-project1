c6c0513e31a7b8f6b57cb78f2c33b24d21f26002
VinhCongLe <vinh223114@student.nctu.edu.vn>
2025-10-21 21:29:19 +0700

    Hoàn thành HĐ4: frontend React + kết nối API GET/POST

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import UserList from "./UserList";
+import AddUser from "./AddUser";
+
+function App() {
+  return (
+    <div>
+      <AddUser />
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
