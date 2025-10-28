467f60344cc8f829a033fca303c4ced747eca360
VinhCongLe <vinh223114@student.nctu.edu.vn>
2025-10-28 10:03:41 +0700

    Lưu thay đổi User.js trước khi checkout

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,9 +5,6 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, default: "user" },
-  avatar: { type: String, default: "" },
-  resetPasswordToken: String,
-  resetPasswordExpires: Date,
 });
 
 module.exports = mongoose.model("User", userSchema);
