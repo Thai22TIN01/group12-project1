182e79abb0230f12ccee28b3b049ff64e2972270
VinhCongLe <vinh223114@student.nctu.edu.vn>
2025-11-08 19:05:50 +0700

    đã xóa các imort api

--- frontend/src/AddUser.jsx
+++ frontend/src/AddUser.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import api from "./api"; // ✅ dùng axios instance đã cấu hình
+ // ✅ dùng axios instance đã cấu hình
 
 function AddUser() {
   const [name, setName] = useState("");
